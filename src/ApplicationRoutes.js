601d8ba71335cb5364547de171c58b60ab3d1b58
mathesbh <mathesbh@gmail.com>
2020-11-16 10:43:11 -0300

    Including route movie details

--- src/ApplicationRoutes.js
+++ src/ApplicationRoutes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Home } from './components/Home'
+import { MovieDetails } from './components/MovieDetails'
 import  Movies  from './components/Movies'
 
 
 export const ApplicationRoutes = () => (
     <>
         <Route path="/" exact component={Home} />
-        <Route path="/movies" exact component={Movies} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/movie/:id" component={MovieDetails} />
     </>
 )
