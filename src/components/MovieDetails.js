4c6a65c60ebbd66e8e8da464cdbc7222e18ffab0
mathesbh <mathesbh@gmail.com>
2020-12-01 20:26:52 -0300

    Refatoring check your favorite movie

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { MoviesService } from '../api/MoviesService'
-import { favoriteMovie } from '../actions/favoriteActions'
-import { useDispatch, useSelector } from 'react-redux'
+import { FavoriteButton } from './FavoriteButton'
+import { FavoriteAdded } from './FavoriteAdded'
 
 
 export const MovieDetails = (props) => {
@@ -14,23 +15,19 @@ export const MovieDetails = (props) => {
     useEffect(() => {
         reqMovie()
     }, [])
-
-    const dispatch = useDispatch()
+ 
     const favorites = useSelector(state => state.favorite.favorite)
+
+    const hasFavorite = favorites.filter(m => m.playload === movie.data.title).length !== 0
 
     return (
         <div className="container">
             <h1>Movie Details</h1>
             <h2>{movie.data.title} | { movie.data.release_date}</h2>
             <h3>Overview:  { movie.data.overview }</h3>
-            <p>Votes: { movie.data.vote_average }</p>
-            <button onClick={() => dispatch(favoriteMovie(movie.data.title))}>Favorite Movie</button>
+            <p>Votes: { movie.data.vote_average }</p>  
             <span>
-                { favorites.map(m => {
-                    if(m.playload === movie.data.title){
-                        return <strong> This movie is included in the favorites movies list!</strong>
-                    } 
-                })}
+                { hasFavorite ? <FavoriteAdded /> : <FavoriteButton movie={movie}/> }
             </span>
         </div>
     )
